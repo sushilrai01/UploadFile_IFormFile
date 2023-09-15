52443ebaf228de14c06950dc354c2238551d04b5
sushilrai01 <ssilray16@gmail.com>
2023-09-15 10:50:42 +0545

    Add project files.

---
+++ UploadFileIForm/Controllers/UploadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFileIForm.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UploadFileIForm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UploadFileIForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
